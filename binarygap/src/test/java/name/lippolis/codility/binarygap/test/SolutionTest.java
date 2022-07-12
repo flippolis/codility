61d8154c1240de9196077acbd622441eeb94ad25
Fabrizio Lippolis <fabrizio.lippolis@aurigaspa.com>
2022-07-12 16:08:47 +0200

    Added test class for binary gap exercise

--- binarygap/src/test/java/name/lippolis/codility/binarygap/test/SolutionTest.java
+++ binarygap/src/test/java/name/lippolis/codility/binarygap/test/SolutionTest.java
@@ -1,7 +1,15 @@
 package name.lippolis.codility.binarygap.test;
 
-public class Test {
+import name.lippolis.codility.binarygap.Solution;
+import org.junit.jupiter.api.Test;
 
-    
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    @Test
+    void test101001000() {
+        assertTrue(2 == new Solution().solution(328));
+    }
 
 }
