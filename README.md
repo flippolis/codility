d4677f8c973b9825999a7e30f815941bc4b92f16
Fabrizio Lippolis <fabrizio.lippolis@aurigaspa.com>
2022-07-12 14:53:29 +0200

    Solution for binary gap exercise

---
+++ README.md
@@ -0,0 +1,3 @@
+# Codility
+
+A personal collection of solutions of programming exercises from Codility (https://app.codility.com/programmers/).
---
+++ binarygap/src/main/java/name/lippolis/codility/binarygap/Solution.java
@@ -0,0 +1,37 @@
+package name.lippolis.codility.binarygap;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public int solution(int n) {
+        String nb = Integer.toBinaryString(n);
+        int bg = 0;
+
+        Pattern pattern = Pattern.compile("1(0*)1");
+        Matcher matcher = pattern.matcher(nb);
+        while (matcher.find()) {
+            if (matcher.groupCount() > 0) {
+                String group = matcher.group(1);
+                System.out.println("Group: " + group);
+
+                if (bg < group.length()) {
+                    bg = group.length();
+                }
+            }
+
+            if (matcher.end() < nb.length() && nb.charAt(matcher.end()) != '1') {
+                matcher = matcher.region(matcher.end() - 1, nb.length());
+            }
+        }
+
+        return bg;
+    }
+
+    public static void main(String[] args) {
+        int s = new Solution().solution(32);
+        System.out.println("Solution: " + s);
+    }
+
+}
