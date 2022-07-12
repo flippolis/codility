3819d3454a48017db7e8a38e51682b0a5d16f86f
Fabrizio Lippolis <fabrizio.lippolis@aurigaspa.com>
2022-07-12 16:35:49 +0200

    Changed assert methods to assertEquals. Added test cases.

--- binarygap/src/test/java/name/lippolis/codility/binarygap/test/SolutionTest.java
+++ binarygap/src/test/java/name/lippolis/codility/binarygap/test/SolutionTest.java
@@ -3,13 +3,23 @@ package name.lippolis.codility.binarygap.test;
 import name.lippolis.codility.binarygap.Solution;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
 
     @Test
     void test101001000() {
-        assertTrue(2 == new Solution().solution(328));
+        assertEquals(2, new Solution().solution(328));
+    }
+
+    @Test
+    void test10101001000001() {
+        assertEquals(5, new Solution().solution(10817));
+    }
+
+    @Test
+    void test10101000010001() {
+        assertEquals(4, new Solution().solution(10769));
     }
 
 }
